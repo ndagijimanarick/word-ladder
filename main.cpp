0a33e376e5ad3cb7c59010125c653d2bcccb6a92
ndagijimanarick <84913876+ndagijimanarick@users.noreply.github.com>
2022-11-10 21:51:40 +0200

    Update main.cpp
    updated codes

--- main.cpp
+++ main.cpp
@@ -6,25 +6,25 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     string word1, word2;
     cout << "Please enter the string of the same length" << endl;
-    cout << "Word 1: ";
+    cout << "first 1: ";
     cin >> word1;
-    while (word1.length() != 4) {
+    while (first.length() != 4) {
         cerr << "\nERROR: Please enter only four letter words\n";
-        cout << "Word 1: ";
-        cin >> word1;
+        cout << "first 1: ";
+        cin >> first;
     }
     cout << "OK well obeyed" << endl;
-    cout << endl << "Word 2: ";
+    cout << endl << "second 2: ";
     cin >> word2;
     while (word2.length() != 4) {
         cerr << "\nERROR: Please enter only four letter words\n";
-        cout << "Word 2: ";
-        cin >> word2;
+        cout << "second 2: ";
+        cin >> second;
     }
     cout << "OK well obeyed" << endl;
-    for (int i = 0; i < word1.length(); i++) {
-        word1[i] = word1[i] & 0xDF;
-        word2[i] = word2[i] & 0xDF;
+    for (int i = 0; i < first 1.length(); i++) {
+        first 1[i] = first[i] & 0xDF;
+        second[i] = second[i] & 0xDF;
     }
     cout << word1 << endl << word2 << endl;
     ifstream inFile;
@@ -34,17 +34,17 @@ int main(int argc, const char * argv[]) {
         exit(1);
     }
     string word;
-    bool word1Found = false;
-    bool word2Found = false;
+    bool first1 Found = false;
+    bool second2 Found = false;
 
-    while (!inFile.eof() && (word1Found != true || word2Found != true)) {
+    while (!inFile.eof() && (first1Found != true || second2Found != true)) {
         inFile >> word;
-        if (word1 == word) word1Found = true;
-        if (word2 == word) word2Found = true;
+        if (first1 == word) first1Found = true;
+        if (second2 == word) second2Found = true;
     }
     inFile.close();
 
-    if (word1Found == true && word2Found == true) {
+    if (first1Found == true && second2Found == true) {
         cout << endl << "Both words are found in the dictionary" << endl;
     }
 
